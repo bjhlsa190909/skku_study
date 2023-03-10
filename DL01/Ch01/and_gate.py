806f1569986e1e099ed01634471595d9d205e9ae
bjhlsa190909 <125746059+bjhlsa190909@users.noreply.github.com>
2023-03-10 22:23:47 +0900

    Update and_gate.py

--- DL01/Ch01/and_gate.py
+++ DL01/Ch01/and_gate.py
@@ -1,6 +1,8 @@
 def AND(x1, x2):
-  w1, w2, theta = 0.5, 0.5, 0.7
-  tmp = x1*w1 + x2*w2
+  x = np.array([x1, x2])
+  w = np.array([0.5, 0.5])
+  b = -0.7
+  tmp = np.sum(x*w) + b
   if tmp <= theta:
     return 0
   elif tmp > theta:
