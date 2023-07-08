d016377e2de9cc118cd54928595c8863e0ffca39
bjhlsa190909 <125746059+bjhlsa190909@users.noreply.github.com>
2023-07-08 15:07:51 +0900

    Update common_layer.py

--- DL_scratch02/ch01/common_layer.py
+++ DL_scratch02/ch01/common_layer.py
@@ -16,3 +16,25 @@ class MatMul:
     dW = np.matmul(self.x.T, dout)
     self.grads[0][...] = dW
     return dx
+
+class Affine:
+  def __init__(self, W, b):
+    self.params = [W, b]
+    self.grads = [np.zeros_like(W), np.zeros_like(b)]
+    self.x = None
+
+  def forward(self, x):
+    W, b = self.params
+    out = np.matmul(x, W) + b
+    self.x = x
+    return out
+
+  def backward(self, dout):
+    W, b = self.params
+    dx = np.matmul(dout, W.T)
+    dW = mp.matmul(self.x.T, dout)
+    db = np.sum(dout, axis = 0)
+
+    self.grads[0][...] = dW
+    self.grads[0][...] = db
+    return dx
