e87909fbca968e0c7e64f2a620192c1ea009ea59
bjhlsa190909 <125746059+bjhlsa190909@users.noreply.github.com>
2023-07-21 21:54:46 +0900

    Create common_util.py

---
+++ DL_scratch02/ch03/common_util.py
@@ -0,0 +1,12 @@
+def create_context_target(corpus, window_size = 1):
+  target = corpus[window_size: -window_size]
+  contexts = []
+
+  for idx in range(window_size, len(corpus) - window_size):
+    cs = []
+    for t in range(-window_size, window_size +1):
+      if t == 0:
+        continue
+      cs.append(corpus[idx + t])
+
+  return np.array(contexts), np.array(target)
